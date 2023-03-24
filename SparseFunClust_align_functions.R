5b6ac7a4dc042c6a5bfcf5207439c07dde93bfe2
Valeria Vitelli <valeria.vitelli@medisin.uio.no>
2023-03-24 15:43:02 +0100

    Added the functions from the no-alignment case explicitly (can be adapted to this case directly, if needed)

--- SparseFunClust_align_functions.R
+++ SparseFunClust_align_functions.R
@@ -1,4 +1,51 @@
-source('SparseFunClust_no_align_functions.R')
+GetOptimalW <- function(b, c_star){
+  # b is the function b(x)
+  # c_star is the parameter c* to decide what component is to be set zero
+  # returns the optimal w(x)
+  b_star <- b
+  b_star[which(b <= c_star)] <- 0
+  norm_b_star <- sqrt(sum((b_star)^2), na.rm = TRUE)
+  w <- (1/norm_b_star)*b_star
+  return(w)
+}
+
+GetOptimalClusters <- function(data, K, w, method){
+  # data is the nxp matrix of functions
+  # K is the number of clusters 
+  # w is the function w(x) - vector of length p
+  # method is a string defining the clustering method ('kmea','pam','hier')
+  # returns the cluster assignments
+  qualim=c('kmea','pam','hier')
+  qualem <- pmatch(method,qualim)
+  weighted_data <- scale(data,center=FALSE,scale=1/w)
+  switch(qualem,{
+    km <- kmeans(weighted_data, K)$cluster
+  },{
+    km <- pam(weighted_data, K)$cluster
+  },{
+    km <- cutree(hclust(dist(weighted_data)),K)
+  })
+  return(km)
+}
+
+GetTemplates <- function(data, clusters, w){
+  # data is the n x p matrix of functions
+  # clusters is the vector of cluster assignments to K clusters - vector of length n
+  # w is the function w(x) - vector of length p
+  # returns the K x p matrix of cluster templates
+  mytmp <- colMeans(data, na.rm = TRUE)
+  ind.mod <- which(w!=0)
+  template <- NULL
+  K <- max(unique(clusters))
+  for(k in 1:K){
+    ksel <- which(clusters==k)
+    mytmp2 <- colMeans(data[ksel,,drop=FALSE], na.rm = TRUE)
+    mytmp[ind.mod] <- mytmp2[ind.mod]
+    template <- rbind(template, mytmp)
+  }
+  
+  return(template)  
+}
 
 ## function to use with L2 distance:
 GetWCSSalign <- function(Y, Cs, warp, xreg, Xall){
