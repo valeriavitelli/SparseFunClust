50696da3a330f6cc40c56b4c062eee0780ef52d3
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-03-24 15:16:31 +0100

    Resolved imported function calls
    Assumes `pam()` refers to `cluster::pam()`.

---
+++ R/SparseFunClust-package.R
@@ -0,0 +1,5 @@
+#' @importFrom grDevices dev.new
+#' @importFrom graphics lines matplot par
+#' @importFrom stats approx cutree dist hclust kmeans loess predict rnorm
+#' @importFrom cluster pam
+NULL
