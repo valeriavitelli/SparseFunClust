5b0bd3e45cf2421ae84fb22fc8d46bd06e68c609
Valeria Vitelli <valeria.vitelli@medisin.uio.no>
2023-03-27 09:59:42 +0200

    Removed functions that can be loaded from the non-alingment case

--- SparseFunClust_align_functions.R
+++ SparseFunClust_align_functions.R
@@ -1,51 +1,4 @@
-GetOptimalW <- function(b, c_star){
-  # b is the function b(x)
-  # c_star is the parameter c* to decide what component is to be set zero
-  # returns the optimal w(x)
-  b_star <- b
-  b_star[which(b <= c_star)] <- 0
-  norm_b_star <- sqrt(sum((b_star)^2), na.rm = TRUE)
-  w <- (1/norm_b_star)*b_star
-  return(w)
-}
-
-GetOptimalClusters <- function(data, K, w, method){
-  # data is the nxp matrix of functions
-  # K is the number of clusters 
-  # w is the function w(x) - vector of length p
-  # method is a string defining the clustering method ('kmea','pam','hier')
-  # returns the cluster assignments
-  qualim=c('kmea','pam','hier')
-  qualem <- pmatch(method,qualim)
-  weighted_data <- scale(data,center=FALSE,scale=1/w)
-  switch(qualem,{
-    km <- kmeans(weighted_data, K)$cluster
-  },{
-    km <- pam(weighted_data, K)$cluster
-  },{
-    km <- cutree(hclust(dist(weighted_data)),K)
-  })
-  return(km)
-}
-
-GetTemplates <- function(data, clusters, w){
-  # data is the n x p matrix of functions
-  # clusters is the vector of cluster assignments to K clusters - vector of length n
-  # w is the function w(x) - vector of length p
-  # returns the K x p matrix of cluster templates
-  mytmp <- colMeans(data, na.rm = TRUE)
-  ind.mod <- which(w!=0)
-  template <- NULL
-  K <- max(unique(clusters))
-  for(k in 1:K){
-    ksel <- which(clusters==k)
-    mytmp2 <- colMeans(data[ksel,,drop=FALSE], na.rm = TRUE)
-    mytmp[ind.mod] <- mytmp2[ind.mod]
-    template <- rbind(template, mytmp)
-  }
-  
-  return(template)  
-}
+source('./SparseFunClust_no_align_functions.R')
 
 ## function to use with L2 distance:
 GetWCSSalign <- function(Y, Cs, warp, xreg, Xall){
@@ -99,7 +52,6 @@ GetWCSSalign <- function(Y, Cs, warp, xreg, Xall){
   return(list(wcss.perfeature=wcss.perfeature, wcss=sum(wcss.perfeature), bcss.perfeature=bcss.perfeature))
 }
 
-
 ## function to use with H1 similarity:
 GetWCSSalignRho <- function(Y, Cs, warp, xreg, Xall){
   # Y    is the nxp matrix of functions
@@ -148,8 +100,6 @@ GetWCSSalignRho <- function(Y, Cs, warp, xreg, Xall){
   return(list(wcss.perfeature=wcss.perfeature, wcss=sum(wcss.perfeature)))
 }
 
-
-
 ### 4 functions to re-approx the registered data
 matapprox.x <- function(X, xout){# abscissa
   Xnew <- matrix(NA, dim(X)[1], length(xout))
@@ -173,8 +123,3 @@ matapprox.y <- function(X, y, xout){# "all-to-one" function
   for(i in 1:dim(X)[1])Ynew[i,] <- approx(X[i,], y,xout,ties='ordered')$y
   return(Ynew)
 }
-
-### integral approximation (trapezoid method)
-integral <- function(x,y){sum((y[-1]+y[-length(y)])*diff(x)/2, na.rm=TRUE)}
-### L2 norm
-L2norm <- function(x,y){sqrt(integral(x,y^2))}
