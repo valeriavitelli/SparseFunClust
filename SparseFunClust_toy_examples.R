1e7cfa14df185abd97ef33a37ec30833b86f5669
Valeria Vitelli <valeria.vitelli@medisin.uio.no>
2023-03-27 10:53:32 +0200

    Few minor changes

--- SparseFunClust_toy_examples.R
+++ SparseFunClust_toy_examples.R
@@ -6,32 +6,42 @@
 
 ## load source files
 source('SparseFunClust_data_generation.R')
+source('SparseFunClust.R')
 source('SparseFunClust_no_align_functions.R')
 source('SparseFunClust_no_align.R')
 
 ## generate the data
-n <- 100
-x <- seq(0,1,len=1000)
+set.seed(24032023)
+n <- 50
+x <- seq(0,1,len=500)
 out <- generate.data.FV17(n, x)
 data <- out$data
 trueClust <- out$true.partition
+matplot(x, t(data), type='l', col=trueClust, 
+        xlab = 'x', ylab = 'data', main = 'Simulated data')
 
 ## run Sparse Functional Clustering (no alignment)
 K <- 2           # run with 2 groups only
 method <- 'kmea' # version with K-means clustering
-m.tuned <- FKMSparseClustering.permute(data, x, K, method=method)$m
-result <- FKMSparseClustering(data, x, K, m.tuned, method)
+tuning.m <- TRUE # perform tuning of the sparsity parameter 
+result <- SparseFunClust(data, x, K = K, do.alignment = FALSE, 
+                         clust.method = method, tuning.m = tuning.m)
+  
 
 ## plot / explore results
-table(trueClust,result$CLUSTER)
-cer(trueClust,result$CLUSTER)
+table(trueClust,result$labels)
+# trueClust  1  2
+# 1  0 50
+# 2 47  3
+cer(trueClust,result$labels) # 0.05878788
 
-x11()
+pdf('myplot.pdf', width = 7, height = 5)
 par(mfrow=c(1,2))
-matplot(x,t(data),type='l',lty=1,col=result$CLUSTER+1,ylab='',
-        main='SparseFunClust - clustering results')
-lines(x,colMeans(data[which(result$CLUSTER==1),]),lwd=2)
-lines(x,colMeans(data[which(result$CLUSTER==2),]),lwd=2)
-plot(x,result$W,type='l',lty=1,lwd=2,ylab='',main='SparseFunClust - Estimated Weighting function')
+matplot(x,t(data),type='l',lty=1,col=result$labels+1,ylab='',
+        main='clustering results')
+lines(x,colMeans(data[which(result$labels==1),]),lwd=2)
+lines(x,colMeans(data[which(result$labels==2),]),lwd=2)
+plot(x,result$w,type='l',lty=1,lwd=2,ylab='',
+     main='estimated weighting function')
 abline(v=0.5)
-
+dev.off()
