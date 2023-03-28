624ebc8eeec2dcbca06d8a1b9de00358cf63516f
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-03-28 06:31:20 +0200

    Exported data-gen functions, added docs to them

--- R/SparseFunClust_data_generation.R
+++ R/SparseFunClust_data_generation.R
@@ -1,5 +1,27 @@
 ## USEFUL FUNCTIONS
 ## 1. CER function
+#' @title CER function
+#' @description Given two partitions P and Q, \code{cer(P, Q)} measures how well
+#' they agree,
+#' the lower the better. It is rigorously defined as the proportion of pairwise
+#' disagreements in the two partitions (i.e., how many, out of all the possible
+#' couples of elements in the sample, are localized in the same cluster in one
+#' partition and in a different one in the other partition).
+#' @param P first vector of cluster assignments (length n)
+#' @param Q second vector of cluster assignments (length n)
+#' @return The CER index, which is a number between 0 and 1, and also equal to
+#' 1 - Rand index (Rand, 1971), a popular measure of the goodness of a
+#' clustering.
+#' @references Rand, W. M. (1971). Objective criteria for the evaluation of
+#' clustering methods. Journal of the American Statistical association, 66(336),
+#' 846-850.
+#' @export
+#' @examples
+#' set.seed(8988327)
+#' x <- seq(0, 1, len = 500)
+#' out <- generate.data.FV17(50, x)
+#' result <- SparseFunClust(out$data, x, K = 2, do.alignment = FALSE)
+#' cer(out$true.partition, result$labels)
 cer <- function(P,Q){
   if(length(P)!=length(Q)){stop('le due partizioni devono avere la stessa lunghezza')}
   cer.comp <- 0
@@ -8,22 +30,23 @@ cer <- function(P,Q){
   return(cer.comp)
 }
 
-
-
-## DATA GENERATION 1: NO-MISALIGNMENT CASE
+#' @title Data generation: no-misalignment case
+#' @description this function generates a set of simulated functional data in
+#' 2 clusters that reproduce the examples in Simulations 2A and 2B in
+#' Floriello & Vitelli (2017).
+#' @param n number of curves
+#' @param x curves' domain
+#' @param paramC proportion of cluster overlap (default 0.5, as in Simulation 2A)
+#' @param plots boolean; should plots be drawn (\code{FALSE} default)
+#' @return a list including:
+#' \itemize{
+#'  \item{\code{$data} matrix (n x \code{length(x)}) with the simulated data}
+#'  \item{\code{$true.partition} vector (length = n) with the true cluster assignments}
+#' }
+#' @export
+#' @examples
+#' generate.data.FV17(5, seq(0, 1, len = 3))
 generate.data.FV17 <- function(n, x, paramC=0.5, plots=FALSE){
-  # this function generates a set of simulated functional data in 2 clusters
-  # that reproduce the examples in Simulations 2A and 2B in Floriello & Vitelli (2017)
-  
-  # n = number of curves
-  # x = curves' domain
-  # paramC = proportion of cluster overlap (default 0.5, as in Simulation 2A)
-  # plots = boolean; should plots be drawn (FALSE default)
-  
-  # returns a list including:
-  # $data = matrix (n x length(x)) with the simulated data
-  # $true.partition = vector (length = n) with the true cluster assignments
-
   a <- 3
   b <- 0
   bpert <- .5
